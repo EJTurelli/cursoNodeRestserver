ac615856c7dc18a3e7e9ffdf3fa09bdec150040b
ejturelli <ejturelli@yahoo.com>
2020-09-07 16:49:10 -0300

    Variable para la conexion a Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -10,7 +10,7 @@ let urlDB = null;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://ejturelli:KWJhrd4Ioqitgloy@cluster0.6keew.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
